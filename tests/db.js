b981678af0ef11649d0579ef3ed40be69b0feb3d
Wagner Cadena <99290876+wcadenacasabaca@users.noreply.github.com>
2025-08-15 10:16:57 -0500

    Update db.js

--- tests/db.js
+++ tests/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 
 // Configura tu conexión a la base de datos
 const connection = mysql.createConnection({
-  host: 'qasugarcrm.cmwu94p44jlr.us-east-2.rds.amazonaws.com',
-  user: 'admin',
-  password: 'JgfsR6HILiJNU1',
-  database: 'postvetas_centra_v3'
+  host: '',
+  user: '',
+  password: '',
+  database: ''
 });
 
 connection.connect((err) => {
